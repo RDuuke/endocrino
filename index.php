3b33e874d95fdf306d4b16c3e310a12ab72d82fe
rduuke <juuanduuke@gmail.com>
2019-09-10 15:58:05 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hiperprolactinemia y prolactinoma</title>
+    <link rel="stylesheet" href="css/lib/reset.css">
+    <link rel="stylesheet" href="css/template.css">
+</head>
+<body>
+<section id="slider" class="p-relative">
+    <div class="container">
+        <div class="logo text-center">
+            <img class="logo-img" src="images/logo.png" alt="">
+        </div>
+        <div class="title flex align-items-center">
+            <img src="images/mujer.png" alt="" width="500">
+            <h1 class="color-pink big-text">Hiperprolactinemia</h1>
+        </div>
+    </div>
+    <div class="description p-absolute bg-blue-transparent full-w">
+        <div class="container color-white">
+            <div class="text-espacio"></div>
+            <div class="text-description">
+                <p>La hiperprolactinemia es un trastorno endocrino común al cual el médico de atención primaria se enfrenta en su
+                    práctica clínica y hace parte de los diagnósticos diferenciales de motivos de consulta frecuentes, especialmente en la
+                    población femenina (alteraciones del ciclo menstrual, infertilidad y galactorrea).
+                </p>
+                <p>El objetivo de este material es servir como guía para el enfoque, en el ámbito de la atención primaria, para de aquellos
+                    pacientes con una posible hiperprolactinemia. Para ello se presentan las siguientes secciones:
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="navbottom">
+    <div class="container">
+        <div class="navbar">
+            <div class="nav-item">
+                <a href="">Link 1</a>
+            </div>
+            <div class="nav-item">
+                <a href="">Link 2</a>
+            </div>
+            <div class="nav-item">
+                <a href="">Link 3</a>
+            </div>
+            <div class="nav-item">
+                <a href="">Link 4</a>
+            </div>
+            <div class="nav-item">
+                <a href="">Link 5</a>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="js/template.js"></script>
+</body>
+</html>
